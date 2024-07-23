d75089d969f1a8d6f97459879b1c3afc8ce09e8b
Mohab Gabber <mohabgabber0@gmail.com>
2024-07-23 03:39:39 +0300

    removed permission change

--- fphlcore/settings.py
+++ fphlcore/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     # * Apps
